98819ef1b490010db074eab32a465fa26a624444
Vaibhhav Bisht <vaibhavbisht06@gmail.com>
2019-04-24 23:53:11 +0530

    Third Commit

--- second_app/lib/main.dart
+++ second_app/lib/main.dart
@@ -1,60 +1,61 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp( new Myapp());
-}
 
-class Myapp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Second App",
-      home: new HomePage(),
-    );
-  }
-}
+ void main(){
+   runApp(new Myapp());
+ }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
+ class Myapp extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       title: " My secound App",
+       home: new Homepage(),
+     );
+   }
+ }
 
+ class Homepage extends StatefulWidget {
+   @override
+   _HomepageState createState() => _HomepageState();
+ }
+ 
+ class _HomepageState extends State<Homepage> {
+   String mytext="Hello Users";
+   void changeText(){
+     setState(() {
+       if(mytext.startsWith("H")){
+         mytext="You have Clicked the button";
+       }
+       else{
+         mytext="Hello Users";
+       }
+     });
+   }
 
-class _HomePageState extends State<HomePage> {
-  String mytext= "Hello World";
-  void changeText(){
-  setState(() {
-    if(mytext.startsWith("H")) {
-      mytext = "Welcome to My App";
-    }
-    else {
-      mytext="Hello User";
-    }
-  });
-  }
-
-  Widget bodyWidget(){
-    return new Container(
-      padding:EdgeInsets.all(8.0),
-      child: new Center(
-        child: new Column(
-          children: <Widget>[
-            new Text(mytext),
-            new RaisedButton(
-              child: new Text("Click"),
-              onPressed:changeText ,
-            )
-          ],
-        ),
-      ),
-    );
-  }
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Home Page"),
-      ),
-      body:bodyWidget()
-    );
-  }
-}
+   Widget ClickButtonWidget(){
+     return new Container(
+       padding: EdgeInsets.all(8.0),
+       child: new Center(
+         child: new Column(
+           children: <Widget>[
+             new Text(mytext),
+             new RaisedButton(
+               child: new Text("Click Me :)"),
+               onPressed: changeText,
+             )
+           ],
+         ),
+       ),
+     );
+   }
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       appBar:  new AppBar(
+         title: new Text("Flutter Pages"),
+       ),
+       body: ClickButtonWidget()
+     );
+   }
+ }
+ 
